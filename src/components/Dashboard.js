848fa32679b807fb0fe15fc6b10618c078dc10ed
Manav Bhojak <manuboojuk@gmail.com>
2021-08-21 01:31:10 -0400

    Initial commit

---
+++ src/components/Dashboard.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { Button } from 'react-bootstrap'
+import { useAuth } from '../contexts/AuthContext'
+import { Link, useHistory } from 'react-router-dom'
+
+export default function Dashboard() {
+
+    const [error, setError] = useState('')
+    const { currentUser, logout } = useAuth()
+    const history = useHistory()
+
+    async function handleLogout() {
+        try {
+            await logout()
+            history.pushState('/login')
+        } catch {
+            setError('Failed to log out')
+        }
+    }
+
+    return (
+        <div>
+            <h1>Dashboard!!!!!!!</h1>
+            <Button variant="link" onClick={handleLogout}>Log Out</Button>
+        </div>
+    )
+}
